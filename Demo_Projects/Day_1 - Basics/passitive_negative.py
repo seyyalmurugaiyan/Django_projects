7489e7a05cc4dd4e5a78b53b1e10607839d177e9
seyyalmurugaiyan <se.murugaiyan@gmail.com>
2021-08-13 12:04:50 +0530

    day_1

---
+++ Demo_Projects/Day_1 - Basics/passitive_negative.py
@@ -0,0 +1,5 @@
+number = int(input("Enter Your Number: "))
+if number>=0:
+    print(f"{number} is correct.")
+else:
+    print(f"{number}It is wrong")
---
+++ Demo_Projects/Day_1 - Basics/vote_calculation.py
@@ -0,0 +1,5 @@
+age = int(input("Enter Your Age"))
+if age>=18:
+    print(f"your age is {age} so your eligible for vote. ")
+else:
+    print(f"You should wait for {18 - age} year to vote")
