95e7f939354dd16cda0c82d25ed91e62926ff992
seyyalmurugaiyan <se.murugaiyan@gmail.com>
2021-08-16 11:55:38 +0530

    day_1

---
+++ Demo_Projects/Day_1 - Basics/addcondition.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter your first Number: "))
+number2 = int(input("Enter your second Number: "))
+if number1 == number2:
+    print(f"{number1}+{number2}*2 = {(number1+number2)*2}")
+else:
+    print(f"{number1}+{number2}={number1+number2}")
