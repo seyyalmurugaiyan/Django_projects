2747883fa884ceac0c6dd357a53daa8031e8cf4e
seyyalmurugaiyan <se.murugaiyan@gmail.com>
2021-08-17 11:50:05 +0530

    django

--- Django_projects/myproject/mydemo/urls.py
+++ Django_projects/myproject/mydemo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home, pullrequests
+from .views import home, pullrequests, calculate
 
 urlpatterns=[
     path('',home,name='home'),
-    path('pullrequests/',pullrequests,name="pullrequests")
+    path('pullrequests/',pullrequests,name="pullrequests"),
+    path('calculate',calculate,name="calculate")
 ]
